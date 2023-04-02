296e492c38e3b9acb5ad041bcf49c9840bf71423
Fawaz441 <abdulsalamfawaz4@gmail.com>
2023-04-02 15:46:57 +0100

    refactored files

--- frontend.py
+++ frontend.py
@@ -1,30 +1,35 @@
 import requests
 import os
 import streamlit as st
+from mkv import ask,append_interaction_to_chat_log
+from twilio.twiml.messaging_response import MessagingResponse
+from mkv import ask, append_interaction_to_chat_log
+
 st.title("Talent AI")
 
-BACKEND_URL = "http://127.0.0.1:8000"
-
+session = {}
 input = st.text_area("Type in a message",key='text')
 
-def clear_text():
-    st.session_state["text"] = ""
-
-def get_response(text):
-    res = requests.post(BACKEND_URL,json={"message":text})
-    if res.ok:
-        message = res.json().get("response")
-        return True, message
-    else:
-        print(res.content)
+def ask_question(incoming_msg):
+    '''Ask a question'''
+    try:
+        chat_log = session.get('chat_log')
+        answer = ask(incoming_msg, chat_log)
+        session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                            chat_log)
+        return True, answer
+    except Exception as e:
+        print(e)
         return False, "There was an error"
+
     
 def on_submit():
+    '''On submit button'''
     content = input.lstrip().rstrip()
     if len(content) > 0:
-        success, message = get_response(content)
+        success, message = ask_question(content)
         if success:
-            clear_text()
+            st.session_state["text"] = ""
             st.write(message) 
         else:
             st.error(message)
@@ -32,4 +37,3 @@ def on_submit():
         st.error("Please enter in text")
 
 st.button("Submit",on_click=on_submit)
-
